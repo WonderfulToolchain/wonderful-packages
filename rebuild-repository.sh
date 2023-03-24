9cb90fee161ab0e6bc7302255d995e97f7b07922
Adrian Siekierka <kontakt@asie.pl>
2023-03-24 19:49:43 +0100

    copy any-arch packages to non-x86 architectures

--- rebuild-repository.sh
+++ rebuild-repository.sh
@@ -6,6 +6,10 @@
 ROOT_DIR=$(pwd)
 
 for i in `ls build/packages/`; do
+	if [ "$i" != "x86_64" ]; then
+		cp build/packages/x86_64/*-any.pkg.* build/packages/"$i"/
+	fi
+
 	pushd containers/x86_64
 	podman build -t wonderful-x86_64 .
 	podman run -i -v "$ROOT_DIR":/wf wonderful-x86_64 su -c "cd /wf/build/packages/"$i" && ( rm wonderful.* ; repo-add wonderful.db.tar.gz *.pkg.tar* )" wfbuilder
