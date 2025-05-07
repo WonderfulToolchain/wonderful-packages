4d708263c8c66b26420af3edfb441c2a8a9306d7
Adrian Siekierka <kontakt@asie.pl>
2025-05-08 00:28:40 +0200

    wf-lua-posix: fix

--- tool/cmd/build.py
+++ tool/cmd/build.py
@@ -43,7 +43,7 @@ def cmd_build(ctx, args):
 
     for name, targets in tqdm([parse_package_reference(package, all_targets) for package in args.packages]):
         if name not in source_cache.get_package_names():
-            tqdm.write(colored(f"[*] {package} not found for {target}, skipping...", attrs=["bold"]))
+            tqdm.write(colored(f"[*] {name} not found for {target}, skipping...", attrs=["bold"]))
             continue
         srcinfo = source_cache.get_package_by_name(name, targets[0])
         if "any" in srcinfo["arch"]:
@@ -53,7 +53,7 @@ def cmd_build(ctx, args):
                 if target.split("/")[1] in srcinfo["arch"]:
                     package_pairs.append((name, target))
                 else:
-                    tqdm.write(colored(f"[*] {package} not supported on {target}, skipping...", attrs=["bold"]))
+                    tqdm.write(colored(f"[*] {name} not supported on {target}, skipping...", attrs=["bold"]))
                     continue
 
     for package, target in tqdm(package_pairs):
